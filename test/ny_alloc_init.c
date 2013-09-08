55c6c707f145ca9daeabe0eee1c18481328e044d
Mikael Voss <mvs@nyantec.com>
2013-09-08 13:44:46 +0000

    Fix ny_alloc_init test

--- test/ny_alloc_init.c
+++ test/ny_alloc_init.c
@@ -17,12 +17,15 @@ int main(int argc, char *argv[]) {
 	int _ = ny_alloc_init(&alloc, &ny, 1021, 23);
 	assert(_ == 0);
 	assert(alloc.raw != nil && alloc.raw != MAP_FAILED);
-#if HAVE_MMAP
+#if NY_ALLOC_GUARD
 	assert(alloc.pool == alloc.raw + ny.page_size);
 	assert(alloc.alloc >= 3 * ny.page_size);
-	assert(alloc.alloc % ny.page_size == 0);
+
 #else
 	assert(alloc.pool == alloc.raw);
+#endif
+#if NY_ALLOC_ALIGN
+	assert(alloc.alloc % ny.page_size == 0);
 #endif
 	assert(alloc.free != UINT32_MAX);
 	assert(alloc.size >= 4);
