4fb120bbfc9ace007a644cd762c42ac5b5052a3b
Mikael Voss <mvs@nyantec.com>
2013-10-12 18:23:10 +0000

    util: Correct function name

--- util.c
+++ util.c
@@ -10,6 +10,6 @@
 
 #include <nyanttp/util.h>
 
-size_t align(size_t size, size_t align) {
+size_t ny_util_align(size_t size, size_t align) {
 	return (size + align - (size_t) 1) & ~(align - (size_t) 1);
 }
